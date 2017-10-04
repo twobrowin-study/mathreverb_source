6f32153cb2f9b2b74eee3a4c4949a99e573bf820
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-04 16:00:58 +0300

    Rewrite draw method

--- source/cmathreverbview.h
+++ source/cmathreverbview.h
@@ -20,9 +20,21 @@ public:
 
 void CMathReverbView::draw(CDrawContext *pContext)
 {
-  pContext->setLineWidth(1);
-  pContext->setFillColor(CColor(255, 255, 255, 255)); // белый цвет
-  pContext->drawRect(size, kDrawFilledAndStroked);
+  // --- bitmap, if one
+  if(getDrawBackground())
+  {
+    getDrawBackground()->draw(pContext, size);
+  }
+  else
+  {
+    // --- setup the background rectangle
+    pContext->setLineWidth(1);
+    pContext->setFillColor(CColor(200, 200, 200, 255)); // light grey
+    pContext->setFrameColor(CColor(0, 0, 0, 255)); // black
+
+    // --- draw the rect filled (with grey) and stroked (line around rectangle)
+    pContext->drawRect(size, kDrawFilledAndStroked);
+  }
   setDirty (false);
 }
 
