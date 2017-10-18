e4844ce85871d3c29a57c6254af6a5d75148afcc
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-18 20:30:34 +0300

    Add all params reset

--- source/mathreverbvst3editor.h
+++ source/mathreverbvst3editor.h
@@ -25,9 +25,33 @@ public:
 			if (pControl->getValueNormalized () > 0.5f) // Нажатие кнопки соответвует значению 1.f
         // Сброс параметров
       {
-        // Установка Gain нулю
+        // Установка Gain в исходное значение
         pControl->setTag (kGainId);
-        pControl->setValue (0);
+        pControl->setValue (1.f);
+				// Установка Length в исходное значение
+        pControl->setTag (kLengthId);
+        pControl->setValue (1.f);
+				// Установка Width в исходное значение
+        pControl->setTag (kWidthId);
+        pControl->setValue (1.f);
+				// Установка Height в исходное значение
+        pControl->setTag (kHeightId);
+        pControl->setValue (1.f);
+				// Установка Reflection в исходное значение
+        pControl->setTag (kReflectionId);
+        pControl->setValue (1.f);
+				// Установка XPos в исходное значение
+        pControl->setTag (kXPosId);
+        pControl->setValue (1.f);
+				// Установка YPos в исходное значение
+        pControl->setTag (kYPosId);
+        pControl->setValue (1.f);
+				// Установка ZPos в исходное значение
+        pControl->setTag (kZPosId);
+        pControl->setValue (1.f);
+				// Установка Bypass в исходное значение
+        pControl->setTag (kBypassId);
+        pControl->setValue (0.f);
 				recreateView ();
       }
     VST3Editor::valueChanged (pControl);
