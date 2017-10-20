909967909ada2153cfebb425feb39e0a7542a59a
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-20 17:46:32 +0300

    Fix of bypass in processing

--- source/mathreverbprocess.h
+++ source/mathreverbprocess.h
@@ -27,7 +27,7 @@ SampleType MathReverb::processAudio (SampleType** in, SampleType** out, int32 nu
 		{
 			for (int32 channel = 0; channel < numChannels; channel++, processingIn += in[channel][sample]);
 			processingOut = graph->process (processingIn) * fGain;
-			for (int32 channel = 0; channel < numChannels; channel++, out[channel][sample] = processingIn);
+			for (int32 channel = 0; channel < numChannels; channel++, out[channel][sample] = processingOut);
 		}
 	}
 
