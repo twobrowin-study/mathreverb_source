93434f8ab98a6c4e6f4f6aa3cd9dda670a0c759d
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-22 20:20:02 +0300

    Test of twoline for's

--- source/mathreverbprocess.h
+++ source/mathreverbprocess.h
@@ -26,9 +26,10 @@ SampleType MathReverb::processAudio (SampleType** in, SampleType** out, int32 nu
 		for (int32 sample = 0; sample < sampleFrames; sample++)
 		{
 			// fGain = 0.1f;
-			for (int32 channel = 0; channel < numChannels; channel++, processingIn += in[channel][sample]);
+			for (int32 channel = 0; channel < numChannels; channel++)
+				processingIn += in[channel][sample];
 			// processingOut = /*graph->process (*/processingIn * fGain/*)*/;
-			for (int32 channel = 0; channel < numChannels; channel++/*processingOut*/)
+			for (int32 channel = 0; channel < numChannels; channel++)
 				out[channel][sample] = 0.f;
 		}
 	}
