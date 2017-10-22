91cd5ab519734a63043dde51a1dfe673a10fd20f
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-22 20:28:06 +0300

    ; Fix

--- source/mathreverbprocess.h
+++ source/mathreverbprocess.h
@@ -26,7 +26,7 @@ SampleType MathReverb::processAudio (SampleType** in, SampleType** out, int32 nu
 		for (int32 sample = 0; sample < sampleFrames; sample++)
 		{
 			// fGain = 0.1f;
-			processingOut = 0.f
+			processingOut = 0.f;
 			for (int32 channel = 0; channel < numChannels; channel++)
 				processingIn += in[channel][sample];
 			// processingOut = /*graph->process (*/processingIn * fGain/*)*/;
