d516f3ae633f92b46ceac2a9174567b993922072
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-24 19:48:35 +0300

    Fix of dalay array

--- source/mathreverbgraph.cpp
+++ source/mathreverbgraph.cpp
@@ -18,8 +18,8 @@ MathReverbGraph::MathReverbGraph (SampleRate sampleRate)
   modelApexes = (MathReverbApex*)std::malloc ((size_t) (mNumberOfModelApexes * sizeof (MathReverbApex)));
 
   // Создадим сток - приёмник
-  DelayPoint[] sinkApexDelay = { { modelApexes + 0, 0.5f * sampleRate },
-                                 { modelApexes + 1, 0.5f * sampleRate } };
+  DelayPoint sinkApexDelay [] = { { modelApexes + 0, 0.5f * sampleRate },
+                                  { modelApexes + 1, 0.5f * sampleRate } };
   sinkApex = new MathReverbApex (sinkApexDelay, 1, kNoBuffer);
 
   // Создадим прочие вершины
