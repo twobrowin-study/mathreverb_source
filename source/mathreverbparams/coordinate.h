99374586014774d574d6724d005fffd6892ce1ee
Egor Dubrovin <dubrovin.en@gmail.com>
2017-10-30 22:28:35 +0300

    Update of new returns

--- source/mathreverbparams/coordinate.h
+++ source/mathreverbparams/coordinate.h
@@ -100,9 +100,15 @@ void CoordinateParameter::updateLimit ()
 {
   fLimit = mLimitGiver->getNormalized () * 49.f;
   if ( getNormalized () * 50.f >= fLimit )
+  {
     Parameter::setNormalized ((50.f + fLimit) / 100.f);
+    return;
+  }
   if ( (getNormalized () - 1.f) * 50.f <= - fLimit )
+  {
     Parameter::setNormalized ((50.f - fLimit) / 100.f);
+    return;
+  }
 }
 
 //------------------------------------------------------------------------
