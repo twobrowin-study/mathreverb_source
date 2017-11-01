7b00778b7bdf8832e2cf2f558dbc3546c52d6fae
Egor Dubrovin <dubrovin.en@gmail.com>
2017-11-01 17:53:16 +0300

    Finnale version and add doxyfile

--- source/version.h
+++ source/version.h
@@ -5,20 +5,20 @@
 // Описание версии плагина
 
 // Основная версия
-#define MAJOR_VERSION_STR "0"
-#define MAJOR_VERSION_INT 0
+#define MAJOR_VERSION_STR "1"
+#define MAJOR_VERSION_INT 1
 
 // Дополнительная версия
-#define SUB_VERSION_STR "5"
-#define SUB_VERSION_INT 5
+#define SUB_VERSION_STR "0"
+#define SUB_VERSION_INT 0
 
 // Номер выпуска
-#define RELEASE_NUMBER_STR "1"
-#define RELEASE_NUMBER_INT 1
+#define RELEASE_NUMBER_STR "10"
+#define RELEASE_NUMBER_INT 10
 
 // Номер сборки
-#define BUILD_NUMBER_STR "3"
-#define BUILD_NUMBER_INT 3
+#define BUILD_NUMBER_STR "2563"
+#define BUILD_NUMBER_INT 2563
 
 // Весия с номером сборки (например "1.0.3.342")
 #define FULL_VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
@@ -28,9 +28,9 @@
 
 #define stringOriginalFilename	"mathreverb.vst3" // Название выходного файла
 #if PLATFORM_64
-#define stringFileDescription	"MathReverb VST3-SDK (64Bit)" // Название при 64Bit архитектуре
+#define stringFileDescription	"MathReverb VST3 (64Bit)" // Название при 64Bit архитектуре
 #else
-#define stringFileDescription	"MathReverb VST3-SDK" // Название при 32Bit архитектуре
+#define stringFileDescription	"MathReverb VST3" // Название при 32Bit архитектуре
 #endif
 #define stringCompanyName		"BMSTU\0" // Название учереждения
 #define stringLegalCopyright	"2017 Dubrovin Egor dubrovin.en@gmail.com" // Информация о разработчике
