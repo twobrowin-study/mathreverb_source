496cd6ebd8d04a489f3b1514a696ff690703ec68
Egor Dubrovin <dubrovin.en@gmail.com>
2017-12-17 21:17:31 +0300

    Delete 64 bit support

--- source/mathreverb.cpp
+++ source/mathreverb.cpp
@@ -141,8 +141,8 @@ tresult PLUGIN_API MathReverb::process (ProcessData& data)
 		// Process with help of template
 		if (data.symbolicSampleSize == kSample32)
 			fVuPPM = processAudio<Sample32> ((Sample32**)in, (Sample32**)out, numChannels, data.numSamples);
-		else
-			fVuPPM = processAudio<Sample64> ((Sample64**)in, (Sample64**)out, numChannels, data.numSamples);
+		// else
+		// 	fVuPPM = processAudio<Sample64> ((Sample64**)in, (Sample64**)out, numChannels, data.numSamples);
 	}
 
 	// 3) Send output params changes
