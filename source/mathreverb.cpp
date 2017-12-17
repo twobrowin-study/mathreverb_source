49f6b251a4328e6102ce47ee535f1d35836ff80c
Egor Dubrovin <dubrovin.en@gmail.com>
2017-12-17 21:21:38 +0300

    Move 32 bit to 64 bits

--- source/mathreverb.cpp
+++ source/mathreverb.cpp
@@ -139,7 +139,7 @@ tresult PLUGIN_API MathReverb::process (ProcessData& data)
 		data.outputs[0].silenceFlags = 0;
 
 		// Process with help of template
-		if (data.symbolicSampleSize == kSample32)
+		if (data.symbolicSampleSize == kSample64)
 			fVuPPM = processAudio<Sample32> ((Sample32**)in, (Sample32**)out, numChannels, data.numSamples);
 		// else
 		// 	fVuPPM = processAudio<Sample64> ((Sample64**)in, (Sample64**)out, numChannels, data.numSamples);
